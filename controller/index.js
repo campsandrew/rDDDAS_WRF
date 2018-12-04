5dc0a577c8cb715a5c91fd4faa4f460e5ba3cc37
campsandrew <andrewrcamps@gmail.com>
2018-12-03 23:06:16 -0700

    fixed

--- controller/index.js
+++ controller/index.js
@@ -15,7 +15,7 @@ var watcher = chokidar.watch(config.controller.watch_path, {
 });
 
 watcher
-  .on('addDir', path => console.log(`Directory ${path} has been added`))
+  .on('add', path => console.log(`Directory ${path} has been added`))
   .on('unlinkDir', path => console.log(`Directory ${path} has been removed`))
   .on('error', error => console.log(`Watcher error: ${error}`))
   .on('ready', () => console.log('Initial scan complete. Ready for changes'))
