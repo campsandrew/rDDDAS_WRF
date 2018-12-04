53ca662200ee7dc92738589ae7b90e56c06ad434
campsandrew <andrewrcamps@gmail.com>
2018-12-04 15:27:58 -0700

    wps data compile

--- wps/routes/wps.js
+++ wps/routes/wps.js
@@ -2,8 +2,27 @@ const router = require("express").Router();
 const cmd = require("node-cmd");
 const config = require("../../config");
 
+const unzipFlags = {
+	bz2: "-jxf",
+	gz: "-zxf"
+};
+
 router.post("/new-geog", function(req, res) {
-	console.log(req.body.file);
+	var file = req.body.file;
+	var type = file.split(".").slice(-1)[0].toLowerCase();
+	var dir = config.wps.geog_dir;
+	var hdfs_dir = config.hdfs.geog_dir;
+	var hdfs_cmd = "hdfs dfs -get " + hdfs_dir + " " + dir;
+	
+	// Run hdfs file upload command
+	cmd.get(hdfs_cmd, function(err, data, stderr) {
+		if(!err) {
+			console.log("HDFS: file added to " + dir);
+		} else {
+			console.log("HDFS ERROR: unable to get file from " + hdfs_dir);
+			console.log(stderr);
+		}
+	});
 	
 	res.json({success: true});
 });
