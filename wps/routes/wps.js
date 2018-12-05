c00f09ed62026f3545bb431cd6fe61cc16a7950b
campsandrew <andrewrcamps@gmail.com>
2018-12-04 23:13:17 -0700

    heartbeat

--- wps/routes/wps.js
+++ wps/routes/wps.js
@@ -13,8 +13,10 @@ router.post("/new-geog", function(req, res) {
 	var file = req.body.file;
 	var type = file.split(".").slice(-1)[0].toLowerCase();
 	var dir = config.wps.geog_dir;
+	var host = config.controller.host;
 	var hdfs_dir = path.join(config.hdfs.geog_dir, file);
 	var hdfs_cmd = "hdfs dfs -get " + hdfs_dir + " " + dir;
+	var body = {type: "wps"};
 	
 	// Run hdfs file upload command and unzip new geographical data
 	cmd.run("rm -rf " + path.join(dir, "*"));
@@ -23,18 +25,39 @@ router.post("/new-geog", function(req, res) {
 			var unzip = "tar " + unzipFlags[type] 
 												 + " " + path.join(dir, file) 
 												 + " -C " + dir
-			
 			console.log("HDFS: file added to " + dir);
+			
+			// Proceed to unzipping files
 			cmd.get(unzip, function(err, data, stderr) {
-				var status = ""
-				console.log("Unzip finished")
-				console.log(err, stderr);
+				
+				
+				if(!err) {
+					body.status = "Geographical Data Updated";
+					body.ready = true;
+				} else {
+					body.status = "ERROR: " + stderr;
+					body.ready = false;
+					console.log("UNZIP ERROR: " + stderr);
+				}
+				
+				//Send status message back to controller
+				postRequest(host, "/node", port, body, function(data) {
+					if(!data.success) {
+						console.log("ERROR: no response from controller");
+					}
+				});
 			});
 		} else {
-			console.log("HDFS ERROR: unable to get file from " + hdfs_dir);
-			console.log(stderr);
+			body.status = "ERROR: " + stderr;
+			body.ready = false;
+			console.log("HDFS ERROR: " + stderr);
 			
 			//Send finished status but without updating wps
+			postRequest(host, "/node", port, body, function(data) {
+				if(!data.success) {
+					console.log("ERROR: no response from controller");
+				}
+			});
 		}
 	});
 	
